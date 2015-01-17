c2add3bde6e602ef79a0b59e9455d67864a0ead1
Karter <karter.linux@gmail.com>
2015-01-17 17:53:11 +0100

    Poprawki wygladu

--- SearchingIamges/SearchingIamges/MainWindow.xaml.cs
+++ SearchingIamges/SearchingIamges/MainWindow.xaml.cs
@@ -1,24 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
 
 namespace SearchingIamges
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        public ObservableCollection<BasicImage> ImagesList { get; private set; }
+
+        public List<Accent> AccentColors { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            PrepareImage();
+            comboBoxImages.ItemsSource = ImagesList;
         }
 
+        #region kwantyzacja
         private void image1_Initialized(object sender, EventArgs e)
         {
         }
@@ -110,5 +123,38 @@ namespace SearchingIamges
 
             ProcessImage.Source = BitmapSourceConvert.ToBitmapSource(processImage);
         }
+        #endregion
+
+        #region Events
+        private void comboBoxImages_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var item = (sender as ComboBox).SelectedItem  as BasicImage;
+            if (item != null)
+            {
+                var pathUri = new Uri(string.Format(@"{0}", item.ImageUri),UriKind.Relative);
+                OrginalImage.Source = new BitmapImage(pathUri);
+            }
+
+        }
+        #endregion
+
+        #region Private Methods
+        private void PrepareImage()
+        {
+            ObservableCollection<BasicImage> observableCollection = new ObservableCollection<BasicImage>();
+            string[] filePaths = Directory.GetFiles("Images", "*.jpg");
+
+            foreach (var filePath in filePaths)
+            {
+                var img = new BasicImage()
+                {
+                    ImageUri = filePath,
+                    ImageText = filePath.Substring("Images\\".Count(), filePath.Count() - "Images\\".Count())
+                };
+                observableCollection.Add(img);
+            }
+            ImagesList = observableCollection;
+        }
+        #endregion
     }
 }
